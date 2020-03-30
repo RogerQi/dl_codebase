VAE pipeline roughly working; KL divergence computation needs to be fixed

main/train.py
@@ -115,7 +115,7 @@ def main():
         scheduler.step()
 
     if cfg.save_model:
-        torch.save(model.state_dict(), "mnist_cnn.pt")
+        torch.save(model.state_dict(), "{0}_final.pt".format(cfg.name))
 
 
 if __name__ == '__main__':

modules/loss/loss.py
@@ -19,4 +19,4 @@ class naive_VAE(nn.Module):
     
     def forward(self, output, original_input):
         assert output.shape == original_input.shape
-        return F.kl_div(output, original_input) + 0 # TODO
+        return F.kl_div(output, original_input, reduction = 'mean') + F.mse_loss(output, original_input)

modules/network/auto_encoder/naive_vae.py
@@ -13,7 +13,10 @@ class net(nn.Module):
         self.relu1 = nn.ReLU()
         self.linear2 = nn.Linear(500, 120)
         self.relu2 = nn.ReLU()
-        self.linear3 = nn.Linear(120, 30)
+        self.linear3_mu = nn.Linear(120, 30)
+        self.linear3_sd = nn.Linear(120, 30)
+        # sampling happens here
+        self.standard_normal = torch.distributions.normal.Normal(0, 1)
         self.inv_linear3 = nn.Linear(30, 120)
         self.inv_relu2 = nn.ReLU()
         self.inv_linear2 = nn.Linear(120, 500)
@@ -27,7 +30,13 @@ class net(nn.Module):
         x = self.relu1(x)
         x = self.linear2(x)
         x = self.relu2(x)
-        x = self.linear3(x)
+        mean_vec = self.linear3_mu(x)
+        sd_vec = self.linear3_sd(x)
+        # Sampling with Reparameterization Trick
+        assert mean_vec.shape == sd_vec.shape
+        offset_vec = self.standard_normal.sample(mean_vec.shape).to(mean_vec.device)
+        x = mean_vec + offset_vec * sd_vec
+        # inverse
         x = self.inv_linear3(x)
         x = self.inv_relu2(x)
         x = self.inv_linear2(x)