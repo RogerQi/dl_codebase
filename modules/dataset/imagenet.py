e480529a68d6d5c23e12718e1ad648e35455f7af
RogerQi <twofs0.5@gmail.com>
2020-04-06 00:43:16 -0500

    update imagenet dataset wrapper to use baseset class

--- modules/dataset/imagenet.py
+++ modules/dataset/imagenet.py
@@ -3,9 +3,14 @@ from torchvision import datasets, transforms
 from .baseset import base_set
 
 def get_train_set(cfg):
-    return datasets.ImageNet('~/datasets', split = 'train', download = True,
+    ds = datasets.ImageNet('~/datasets', split = 'train', download = True,
                        transform = transforms.ToTensor())
+    return base_set(ds, "train", cfg)
 
 def get_val_set(cfg):
-    return datasets.ImageNet('~/datasets', split = 'val', download = True,
+    ds = datasets.ImageNet('~/datasets', split = 'val', download = True,
                         transform = transforms.ToTensor())
+    return base_set(ds, "test", cfg)
+
+def get_test_set(cfg):
+    raise ValueError("ImageNet does not come with test set!")
