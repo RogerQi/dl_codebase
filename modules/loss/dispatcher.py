20f77959968de29e69bce2d923bd5a8fb25ec93d
RogerQi <twofs0.5@gmail.com>
2020-05-18 02:31:26 -0500

    fixed loss module bug

--- modules/loss/dispatcher.py
+++ modules/loss/dispatcher.py
@@ -8,11 +8,5 @@ def dispatcher(cfg):
     if loss_name == "cross_entropy":
         from .loss import cross_entropy
         return cross_entropy(cfg)
-    elif loss_name == "naive_vae":
-        from .loss import naive_VAE
-        return naive_VAE(cfg)
-    elif loss_name == "nll_loss":
-        from .loss import nll_loss
-        return nll_loss(cfg)
     else:
         raise NotImplementedError
