2470d7ea22129d160c2e176d2163e97419473041
RogerQi <twofs0.5@gmail.com>
2020-05-25 01:35:49 -0500

    add dummy identity module

--- modules/classifier/dispatcher.py
+++ modules/classifier/dispatcher.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class identity_mod(nn.Module):
+    def forward(self, x):
+        return x
+
 def dispatcher(cfg, feature_size):
     classifier_name = cfg.CLASSIFIER.classifier
     assert classifier_name != "none"
@@ -9,5 +13,8 @@ def dispatcher(cfg, feature_size):
         import classifier.fc as fc
         fc_classifier = fc.net(cfg, feature_size)
         return fc_classifier
+    elif classifier_name == "identity":
+        identity_module = identity_mod()
+        return identity_module
     else:
         raise NotImplementedError
