c564112e6eefb61f0c488759f6710ea6dcb9a9c8
RogerQi <twofs0.5@gmail.com>
2020-05-30 23:28:32 -0500

    support CxHxW label for random transform

--- modules/dataset/transforms/joint_transform_ops.py
+++ modules/dataset/transforms/joint_transform_ops.py
@@ -15,21 +15,30 @@ def joint_random_crop(transforms_cfg):
     def crop(img, target):
         assert img.shape[-2:] == target.shape[-2:]
         assert len(img.shape) == 3, "Only C x H x W images are supported"
-        assert len(target.shape) == 2, "Only H x W labels are supported"
         H, W = img.shape[-2:]
         if H <= output_H or W <= output_W:
             # Pad and crop from zero
             temp_size = (max(H, output_H), max(W, output_W))
             temp_shape = img.shape[:-2] + temp_size
             img_bg = torch.zeros(temp_shape, dtype = img.dtype, device = img.device)
-            target_bg = torch.zeros(temp_size, dtype = target.dtype, device = target.device)
             img_bg[:, 0:H, 0:W] = img
-            target_bg[0:H, 0:W] = target
             img = img_bg
-            target = target_bg
+            if len(target.shape) == 2:
+                target_bg = torch.zeros(temp_size, dtype = target.dtype, device = target.device)
+                target_bg[0:H, 0:W] = target
+                target = target_bg
+            else:
+                assert len(target.shape) == 3, "This block supports C x H x W binary labels"
+                C = target.shape[0]
+                target_bg = torch.zeros((C,) + temp_size, dtype = target.dtype, device = target.device)
+                target_bg[:, 0:H, 0:W] = target
+                target = target_bg
             i = j = 0
         else:
             i = random.randint(0, H - output_H)
             j = random.randint(0, W - output_W)
-        return (img[:, i:i + output_H, j:j + output_W], target[i:i + output_H, j:j + output_W])
+        if len(target.shape) == 2:
+            return (img[:, i:i + output_H, j:j + output_W], target[i:i + output_H, j:j + output_W])
+        else:
+            return (img[:, i:i + output_H, j:j + output_W], target[:, i:i + output_H, j:j + output_W])
     return crop
