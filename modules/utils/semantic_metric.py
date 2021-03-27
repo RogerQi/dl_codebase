afaf3bda82f07e1b2ceb8b7e07bb926f9241ded3
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-03-27 18:01:28 -0500

    update semantic metric so now it evaluates both foreground/background

--- modules/utils/semantic_metric.py
+++ modules/utils/semantic_metric.py
@@ -1,34 +1,36 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from IPython import embed
 
 def compute_pixel_acc(pred, label):
     '''
     pred: BHW
     label: BHW
     '''
-    valid = (label >= 0)
-    acc_sum = (valid * (pred == label)).sum()
-    valid_sum = valid.sum()
-    acc = float(acc_sum) / (valid_sum + 1e-10)
-    return acc, valid_sum
+    assert pred.shape == label.shape
+    acc_sum = (pred == label).sum()
+    acc = float(acc_sum) / (np.prod(pred.shape))
+    return acc, 0
 
-def compute_iou(imPred, imLab, numClass):
-    imPred = np.asarray(imPred).copy()
-    imLab = np.asarray(imLab).copy()
+def compute_iou(pred_map, label_map, num_classes):
+    pred_map = np.asarray(pred_map).copy()
+    label_map = np.asarray(label_map).copy()
 
-    imPred += 1
-    imLab += 1
-    # Remove classes from unlabeled pixels in gt image.
-    # We should not penalize detections in unlabeled portions of the image.
-    imPred = imPred * (imLab > 0)
+    # When computing intersection, all pixels that are not
+    # in the intersection are masked with zeros.
+    # So we add 1 to the existing mask so that background pixels can be computed
+    pred_map += 1
+    label_map += 1
 
     # Compute area intersection:
-    intersection = imPred * (imPred == imLab)
+    intersection = pred_map * (pred_map == label_map)
     (area_intersection, _) = np.histogram(
-        intersection, bins=numClass, range=(1, numClass))
+        intersection, bins=num_classes, range=(1, num_classes))
 
     # Compute area union:
-    (area_pred, _) = np.histogram(imPred, bins=numClass, range=(1, numClass))
-    (area_lab, _) = np.histogram(imLab, bins=numClass, range=(1, numClass))
+    (area_pred, _) = np.histogram(pred_map, bins=num_classes, range=(1, num_classes))
+    (area_lab, _) = np.histogram(label_map, bins=num_classes, range=(1, num_classes))
     area_union = area_pred + area_lab - area_intersection
 
     return np.sum(area_intersection) / np.sum(area_union)
