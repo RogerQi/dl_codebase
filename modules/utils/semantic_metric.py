8ba06b561abc6f0aa98b9631b6b5b48c3ad71b44
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-03-27 21:13:34 -0500

    add optional fg only flag to semantic metrics (this works better in dataset where majority of pixels are unlabelled)

--- modules/utils/semantic_metric.py
+++ modules/utils/semantic_metric.py
@@ -3,19 +3,28 @@ import matplotlib.pyplot as plt
 
 from IPython import embed
 
-def compute_pixel_acc(pred, label):
+def compute_pixel_acc(pred, label, foreground_only=True):
     '''
     pred: BHW
     label: BHW
     '''
     assert pred.shape == label.shape
-    acc_sum = (pred == label).sum()
-    acc = float(acc_sum) / (np.prod(pred.shape))
-    return acc, 0
+    if foreground_only:
+        valid = (label >= 0)
+        acc_sum = (valid * (pred == label)).sum()
+        valid_sum = valid.sum()
+        acc = float(acc_sum) / (valid_sum + 1e-10)
+        return acc, valid_sum
+    else:
+        acc_sum = (pred == label).sum()
+        acc = float(acc_sum) / (np.prod(pred.shape))
+        return acc, 0
 
-def compute_iou(pred_map, label_map, num_classes):
+def compute_iou(pred_map, label_map, num_classes, foreground_only=True):
     pred_map = np.asarray(pred_map).copy()
     label_map = np.asarray(label_map).copy()
+
+    assert pred_map.shape == label_map.shape
 
     # When computing intersection, all pixels that are not
     # in the intersection are masked with zeros.
@@ -33,4 +42,8 @@ def compute_iou(pred_map, label_map, num_classes):
     (area_lab, _) = np.histogram(label_map, bins=num_classes, range=(1, num_classes))
     area_union = area_pred + area_lab - area_intersection
 
-    return np.sum(area_intersection) / np.sum(area_union)
+    if foreground_only:
+        # Remove first bg channel
+        return np.sum(area_intersection[1:]) / (np.sum(area_union[1:]) + 1e-10)
+    else:
+        return np.sum(area_intersection) / np.sum(area_union)
