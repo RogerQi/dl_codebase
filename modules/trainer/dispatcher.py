87123cab4e0b8613cdae7fc881314625302f6d4c
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-07-13 16:20:28 +0800

    implemented and tested seg trainer

--- modules/trainer/dispatcher.py
+++ modules/trainer/dispatcher.py
@@ -6,7 +6,10 @@ def dispatcher(cfg):
     task_name = cfg.task
     assert task_name != "none"
     if task_name == "classification":
-        from .clf_trainer import clf_trainer
-        return clf_trainer
+        from .clf_trainer import clf_trainer as clf_trainer_fn
+        return clf_trainer_fn
+    elif task_name == "semantic_segmentation":
+        from .seg_trainer import seg_trainer as seg_trainer_fn
+        return seg_trainer_fn
     else:
         raise NotImplementedError
