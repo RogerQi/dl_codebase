2ab45c9b7c93e04db643748729020e94477d2aa9
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-07-15 13:47:39 +0800

    add class map func for COCO instance Seg to prepare COCO eval

--- modules/dataset/coco.py
+++ modules/dataset/coco.py
@@ -4,16 +4,20 @@ import json
 import numpy as np
 import torch
 import torchvision
+from copy import deepcopy
 from torchvision import datasets, transforms
 from PIL import Image
 from pycocotools.coco import COCO
 from .baseset import base_set
+
+from IPython import embed
 
 # 2017 train images normalization constants
 #   mean: 0.4700, 0.4468, 0.4076
 #   sd: 0.2439, 0.2390, 0.2420
 
 class COCOSeg(datasets.vision.VisionDataset):
+    CLASS_NAMES_LIST = [] # TODO
     def __init__(self, root, train=True):
         super(COCOSeg, self).__init__(root, None, None, None)
         self.min_area = 0 # 200
@@ -25,9 +29,17 @@ class COCOSeg(datasets.vision.VisionDataset):
 
         # COCO class
         class_list = sorted([i for i in self.coco.cats.keys()])
+
+        # The instances labels in COCO dataset is not dense
+        # e.g., total 80 classes. Some objects are labeled as 82
+        # but they are 73rd class; while none is labeled as 83.
         self.class_map = {}
         for i in range(len(class_list)):
             self.class_map[class_list[i]] = i + 1
+        
+        # Given a class idx (1-80), self.class_map gives the list of images that contain
+        # this class idx
+        self.instance_class_map = self.init_class_map()
 
     def _get_img(self, img_id):
         img_desc = self.coco.imgs[img_id]
@@ -50,9 +62,31 @@ class COCOSeg(datasets.vision.VisionDataset):
                 assert real_class_id >= 0 and real_class_id <= 80
                 seg_mask = torch.max(seg_mask, ann_mask * real_class_id)
         return (img, seg_mask.long())
+    
+    def get_class_map(self, class_id):
+        return deepcopy((self.instance_class_map[class_id]))
 
     def __len__(self):
         return len(self.coco.imgs)
+    
+    def init_class_map(self):
+        """
+        init class map and write as txt to class_map_dir
+        """
+        instances_class_map = {}
+        for c in range(1, 81):
+            instances_class_map[c] = set() # avoid duplicated elements
+        for i in range(len(self)):
+            img_id = self.img_ids[i]
+            annotations = self.coco.imgToAnns[img_id]
+            for ann in annotations:
+                if ann['iscrowd']:
+                    continue # crowded examples are excluded
+                real_class_id = self.class_map[ann['category_id']]
+                instances_class_map[real_class_id].add(i)
+        for c in range(1, 81):
+            instances_class_map[c] = sorted(list(instances_class_map[c]))
+        return instances_class_map
 
 def get_train_set(cfg):
     ds = COCOSeg("/data/COCO2017/", True)
