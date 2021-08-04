ed9b482448c7eeff437ebd932ec916e7f3a44fb7
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-08-04 16:31:42 +0800

    implemented an independent non parametric lib

--- main/live_fs_continual_eval.py
+++ main/live_fs_continual_eval.py
@@ -102,31 +102,6 @@ def test(cfg, model, post_processor, criterion, device, class_names_list, novel_
     cap.release()
     cv2.destroyAllWindows()
 
-def masked_average_pooling(mask_b1hw, feature_bchw, normalization):
-    '''
-    Params
-        - mask_b1hw: a binary mask whose element-wise value is either 0 or 1
-        - feature_bchw: feature map obtained from the backbone
-    
-    Return: Mask-average-pooled vector of shape 1 x C
-    '''
-    if len(mask_b1hw.shape) == 3:
-        mask_b1hw = mask_b1hw.view((mask_b1hw.shape[0], 1, mask_b1hw.shape[1], mask_b1hw.shape[2]))
-
-    # Assert remove mask is not in mask provided
-    assert -1 not in mask_b1hw
-
-    # Spatial resolution mismatched. Interpolate feature to match mask size
-    if mask_b1hw.shape[-2:] != feature_bchw.shape[-2:]:
-        feature_bchw = F.interpolate(feature_bchw, size=mask_b1hw.shape[-2:], mode='bilinear')
-    
-    if normalization:
-        feature_norm = torch.norm(feature_bchw, p=2, dim=1).unsqueeze(1).expand_as(feature_bchw)
-        feature_bchw = feature_bchw.div(feature_norm + 1e-5) # avoid div by zero
-
-    batch_pooled_vec = torch.sum(feature_bchw * mask_b1hw, dim = (2, 3)) / (mask_b1hw.sum(dim = (2, 3)) + 1e-5) # B x C
-    return torch.mean(batch_pooled_vec, dim=0)
-
 def main():
     # --------------------------
     # | Initial set up
@@ -203,7 +178,7 @@ def main():
         target_tensor_bhw = target_tensor_bhw.to(device)
         assert 1 in target_tensor_bhw
         feature = backbone_net(img_tensor_bchw)
-        class_weight_vec = masked_average_pooling(target_tensor_bhw == 1, feature, True).reshape((1, -1, 1, 1))
+        class_weight_vec = utils.masked_average_pooling(target_tensor_bhw == 1, feature, True).reshape((1, -1, 1, 1))
         ori_post_processor_weight = post_processor.pixel_classifier.class_mat.weight.data
         ori_num_classes = ori_post_processor_weight.shape[0]
         post_processor = classifier.dispatcher(cfg, feature_shape, num_classes=ori_num_classes + 1)
--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -1,3 +1,4 @@
 from .cnn_dim_calc import conv_output_shape
 from .semantic_metric import compute_pixel_acc, compute_iou, compute_binary_precision, compute_binary_recall, compute_iu
 from .visualization import generalized_imshow, visualize_segmentation
+from .non_parametric import masked_average_pooling
