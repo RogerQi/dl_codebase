f2b5d04e0ab33a480525469d161268ed47478967
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-08-16 00:15:11 +0800

    use better and self-explanatory task name

--- modules/trainer/dispatcher.py
+++ modules/trainer/dispatcher.py
@@ -14,13 +14,13 @@ def dispatcher(cfg):
     elif task_name == "few_shot_semantic_segmentation_fine_tuning":
         from .fs_ft_seg_trainer import fs_ft_seg_trainer as fs_ft_seg_trainer_fn
         return fs_ft_seg_trainer_fn
-    elif task_name == "incremental_few_shot_segmentation":
+    elif task_name == "GIFS":
         from .GIFS_seg_trainer import GIFS_seg_trainer as GIFS_seg_trainer_fn
         return GIFS_seg_trainer_fn
     elif task_name == "few_shot_classification":
         from .fs_ft_clf_trainer import fs_ft_clf_trainer as fs_ft_clf_trainer_fn
         return fs_ft_clf_trainer_fn
-    elif task_name == "sequential_incremental_few_shot_segmentation":
+    elif task_name == "sequential_GIFS":
         from .sequential_GIFS_seg_trainer import sequential_GIFS_seg_trainer as sequential_GIFS_seg_trainer_fn
         return sequential_GIFS_seg_trainer_fn
     else:
