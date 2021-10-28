74bdf9620f47ea5f4019eb8a5d07b8e8096ab4b5
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-10-28 20:56:50 +0800

    move scheduler stepping code to trainer for per-iteration polynomial training

--- modules/trainer/trainer_base.py
+++ modules/trainer/trainer_base.py
@@ -1,8 +1,9 @@
+import math
 import torch
 
 from abc import abstractmethod
 
-class trainer_base:
+class trainer_base(object):
     def __init__(self, cfg, backbone_net, post_processor, criterion, dataset_module, device):
         self.cfg = cfg
         self.backbone_net = backbone_net
@@ -39,6 +40,18 @@ class trainer_base:
     @abstractmethod
     def live_run(self, device):
         raise NotImplementedError
+    
+    def adapt_scheduler(self, start_epoch, scheduler):
+        assert isinstance(start_epoch, int) and start_epoch >= 1
+        if start_epoch > 1:
+            if self.cfg.TRAIN.step_per_iter:
+                elapsed_iter = math.ceil(len(self.train_set)  / self.cfg.TRAIN.batch_size) * (start_epoch - 1)
+                for _ in range(elapsed_iter):
+                    scheduler.step()
+            else:
+                # Tune LR scheduler
+                for _ in range(1, start_epoch):
+                    scheduler.step()
 
     def save_model(self, file_path):
         """Save default model (backbone_net, post_processor to a specified file path)
