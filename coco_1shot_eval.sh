74547e3ff050088f1fdeff01ae7805dfede99000
RogerQi <17267101+RogerQi@users.noreply.github.com>
2021-11-16 04:38:09 +0800

    add coco 1shot script

---
+++ coco_1shot_eval.sh
@@ -0,0 +1,4 @@
+python3 main/test.py --cfg configs/fs_incremental/coco_20i_1shot/coco20i_split0_1shot.yaml --load /data/cvpr2022/coco20i_split0_BR_BS32.pt
+python3 main/test.py --cfg configs/fs_incremental/coco_20i_1shot/coco20i_split1_1shot.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
+python3 main/test.py --cfg configs/fs_incremental/coco_20i_1shot/coco20i_split2_1shot.yaml --load /data/cvpr2022/coco20i_split2_BR_BS32.pt
+python3 main/test.py --cfg configs/fs_incremental/coco_20i_1shot/coco20i_split3_1shot.yaml --load /data/cvpr2022/coco20i_split3_BR_BS32.pt
