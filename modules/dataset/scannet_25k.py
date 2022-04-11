76a33a660fe1d9a06756783fe3220d134c96fc14
RogerQi <17267101+RogerQi@users.noreply.github.com>
2022-04-11 04:15:56 -0500

    modify ScanNet reader to use 21 classes like PanopticFusion

--- modules/dataset/scannet_25k.py
+++ modules/dataset/scannet_25k.py
@@ -1,9 +1,6 @@
-import sys
 import os
-import json
 import numpy as np
 import torch
-import torchvision
 from torchvision import datasets, transforms
 from PIL import Image
 
@@ -17,12 +14,34 @@ class ScanNet25K(datasets.vision.VisionDataset):
     Data availabel at http://kaldir.vc.in.tum.de/scannet/v2/tasks/scannet_frames_25k.zip
     '''
 
-    CLASS_NAMES_LIST = ["TODO" for i in range(41)]
+    # Only 20+1 classes are used
+    # https://github.com/ScanNet/ScanNet/blob/master/BenchmarkScripts/3d_evaluation/evaluate_semantic_label.py
+    TRAIN_SPLIT_URL = "https://raw.githubusercontent.com/ScanNet/ScanNet/master/Tasks/Benchmark/scannetv2_train.txt"
+    VAL_SPLIT_URL = "https://raw.githubusercontent.com/ScanNet/ScanNet/master/Tasks/Benchmark/scannetv2_val.txt"
+    CLASS_NAMES_LIST = ['background', 'wall', 'floor', 'cabinet', 'bed', 'chair', 'sofa', 'table', 'door', 'window', 'bookshelf', 'picture', 'counter', 'desk', 'curtain', 'refrigerator', 'shower curtain', 'toilet', 'sink', 'bathtub', 'otherfurniture']
+    VALID_CLASS_IDS = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28, 33, 34, 36, 39])
+    NYU_SCANNET_MAPPING = np.zeros(41) # NYU40 has 40 labels
+    for i in range(len(VALID_CLASS_IDS)):
+        NYU_SCANNET_MAPPING[VALID_CLASS_IDS[i]] = i + 1
 
-    def __init__(self, root):
+    def __init__(self, root, train=False):
         super(ScanNet25K, self).__init__(root, None, None, None)
         root = os.path.join(root, "scannet_frames_25k")
-        scene_list = sorted(os.listdir(root))
+
+        train_split_path = os.path.join(root, "train_split.txt")
+        val_split_path = os.path.join(root, "val_split.txt")
+
+        if train:
+            if not os.path.exists(train_split_path):
+                utils.download_file(self.TRAIN_SPLIT_URL, train_split_path)
+            split_path = train_split_path
+        else:
+            if not os.path.exists(val_split_path):
+                utils.download_file(self.VAL_SPLIT_URL, val_split_path)
+            split_path = val_split_path
+        
+        with open(split_path) as f:
+            scene_list = [i.strip() for i in f.readlines()]
 
         self.img_paths = []
         self.target_paths = []
@@ -34,7 +53,7 @@ class ScanNet25K(datasets.vision.VisionDataset):
             label_img_list = sorted(os.listdir(label_dir))
             
             # color and label len should be equal
-            assert len(color_img_list) == len(label_img_list)
+            assert len(color_img_list) == len(label_img_list), f"{scene_name} got {color_img_list} and {label_img_list}"
 
             # assert frame numbers are the same. We can't use == directly because
             # RGB images are stored as .jpg while labels are stored as .png
@@ -60,6 +79,7 @@ class ScanNet25K(datasets.vision.VisionDataset):
 
         target = Image.open(self.target_paths[idx])
         target_np = np.array(target, dtype=np.long)
+        target_np = self.NYU_SCANNET_MAPPING[target_np]
 
         return img, torch.tensor(target_np)
 
@@ -67,9 +87,9 @@ class ScanNet25K(datasets.vision.VisionDataset):
         return len(self.img_paths)
 
 def get_train_set(cfg):
-    ds = ScanNet25K(utils.get_dataset_root())
+    ds = ScanNet25K(utils.get_dataset_root(), train=True)
     return base_set(ds, "train", cfg)
 
 def get_val_set(cfg):
-    ds =ScanNet25K(utils.get_dataset_root())
+    ds =ScanNet25K(utils.get_dataset_root(), train=False)
     return base_set(ds, "test", cfg)
