6f7183d4adefe087d4795923f4c1ea0dde3044e4
RogerQi <17267101+RogerQi@users.noreply.github.com>
2022-09-05 03:32:14 -0500

    remove unused synthesis probability strategy ablation script

--- synthesis_probability_ablations.sh
+++
@@ -1,4 +0,0 @@
-python3 main/test.py --cfg configs/fs_incremental/ablations/copy_paste/CAS.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/ablations/copy_paste/always_no.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/ablations/copy_paste/always.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
-python3 main/test.py --cfg configs/fs_incremental/ablations/copy_paste/vRFS.yaml --load /data/cvpr2022/coco20i_split1_BR_BS32.pt
