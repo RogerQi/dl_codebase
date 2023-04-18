b8cf50fd29e88ff0a11aeb74a60cb3fe6664cb8f
RogerQi <rizhaoq2@illinois.edu>
2023-04-18 13:14:47 -0500

    modify dataset seeker to automatically find data path on Delta/AUTODL

--- modules/utils/misc.py
+++ modules/utils/misc.py
@@ -1,29 +1,3 @@
-# import os
-# import torch
-# import urllib
-#
-# def download_file(url, local_path):
-#     g = urllib.request.urlopen(url)
-#     with open(local_path, 'b+w') as f:
-#         f.write(g.read())
-#
-# def get_dataset_root():
-#     try:
-#         return os.environ['DATASET_ROOT']
-#     except KeyError:
-#         if os.path.exists('/root/autodl-tmp'):
-#             return '/root/autodl-tmp'
-#         elif os.path.exists('/data'):
-#             return '/data'
-#         else:
-#             raise Exception("Data dir not found. Please specify data base dir in $DATASET_ROOT")
-#
-# def guess_device():
-#     if torch.cuda.is_available():
-#         return torch.device('cuda')
-#     else:
-#         return torch.device('cpu')
-
 import os
 import torch
 import urllib
@@ -37,7 +11,14 @@ def get_dataset_root():
     try:
         return os.environ['DATASET_ROOT']
     except KeyError:
-        return "/root/autodl-tmp/data"
+        if os.path.exists('/root/autodl-tmp'):
+            return "/root/autodl-tmp"
+        elif os.path.exists('/data'):
+            return "/data"
+        elif os.path.exists('/scratch/bbqi/illinirm/data'):
+            return "/scratch/bbqi/illinirm/data"
+        else:
+            raise Exception("Please specify dataset root by setting environment variable DATASET_ROOT")
 
 def guess_device():
     if torch.cuda.is_available():
